13f76faea7a23ae9da38c45f30fbc38eb068335a
Robert Schroll <robert@thedataincubator.com>
2023-04-21 20:18:34 +0000

    Make database.py into a proper module
    This introduces a query_db function that we can call.

--- database.py
+++ database.py
@@ -1,16 +1,20 @@
 import sys
 import sqlalchemy
 
-mindepth = float(sys.argv[1])
-mingradient = float(sys.argv[2])
-
 conn_string = "postgresql://tdi:qKmd8s5ze7WAYV@adventureworks.tditrain.com:5431/wells"
-engine = sqlalchemy.create_engine(conn_string)
-conn = engine.connect()
-query = sqlalchemy.text("""
-    SELECT latitude, longitude, depth, gradient
-    FROM wells
-    WHERE depth > :mindepth AND gradient > :mingradient;
-""")
-result = conn.execute(query, {'mindepth': mindepth, 'mingradient': mingradient})
-print(result.fetchall())
+
+def query_db(mindepth, mingradient):
+    engine = sqlalchemy.create_engine(conn_string)
+    conn = engine.connect()
+    query = sqlalchemy.text("""
+        SELECT latitude, longitude, depth, gradient
+        FROM wells
+        WHERE depth > :mindepth AND gradient > :mingradient;
+    """)
+    result = conn.execute(query, {'mindepth': mindepth, 'mingradient': mingradient})
+    return result.fetchall()
+
+if __name__ == '__main__':
+    mindepth = float(sys.argv[1])
+    mingradient = float(sys.argv[2])
+    print(query_db(mindepth, mingradient))
