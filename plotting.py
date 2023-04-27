8957c344f35557fc593d6c0973e01b966819c94e
Robert Schroll <robert@thedataincubator.com>
2023-04-27 17:23:57 +0000

    Add function to generate an altair plot of the well locations

---
+++ plotting.py
@@ -0,0 +1,31 @@
+import altair as alt
+import pandas as pd
+from vega_datasets import data
+
+def plot_wells(coords):
+    df = pd.DataFrame(coords)
+    
+    # Read in polygons from topojson
+    states = alt.topo_feature(data.us_10m.url, feature='states')
+
+    # US states background
+    background = alt.Chart(states).mark_geoshape(
+        fill='lightgray',
+        stroke='white'
+    ).properties(
+        width=500,
+        height=300
+    ).project('albersUsa')
+
+    # airport positions on background
+    points = alt.Chart(df).mark_circle().encode(
+        longitude='longitude',
+        latitude='latitude',
+        color=alt.Color('gradient:Q', scale=alt.Scale(scheme='inferno')),
+        tooltip=[
+            alt.Tooltip('depth', title='Depth (m)'),
+            alt.Tooltip('gradient', title='Gradient (°C/m)', format='0.3f')
+        ]
+    )
+
+    return background + points
